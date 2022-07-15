f842b082401c79718448adecfc0e06e0b988cccf
Renan Torres <rtorres@daitan.com>
2022-07-15 10:05:58 -0300

    [GKR]: Styled LowerToolbar component

--- src/components/MenuPanel/LowerToolbar/index.jsx
+++ src/components/MenuPanel/LowerToolbar/index.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
+import {
+  NoteLowerToolbar,
+  RestoreButton,
+  DeleteButton,
+  AddReminderButton,
+  ColorPaletteButton,
+  ArchiveButton,
+  MenuButton,
+} from './styles';
 
-import Button from '../../Button/index';
 import NoteItemModel from '../../../models/NoteItemModel';
 
 function LowerToolbar({ noteData, handleDataChange, showModal }) {
   return (
-    <div className="note-lower-toolbar">
+    <NoteLowerToolbar>
       {noteData.isTrashed ? (
         <>
-          <Button
-            className="lower-toolbar-button restore-icon"
+          <RestoreButton
             label="Restore Note"
             handleClick={() => handleDataChange({ isTrashed: false })}
           />
-          <Button
-            className="lower-toolbar-button delete-icon"
+          <DeleteButton
             label="Delete Note"
             handleClick={() => handleDataChange({
               noteTime: { ...noteData.noteTime, deletionDate: 0 },
@@ -25,31 +30,26 @@ function LowerToolbar({ noteData, handleDataChange, showModal }) {
         </>
       ) : (
         <>
-          <Button
-            className="lower-toolbar-button add-reminder-icon"
+          <AddReminderButton
             label="Add Reminder"
             handleClick={() => handleDataChange({ isReminder: !noteData.isReminder })}
           />
-          <Button
-            className="lower-toolbar-button color-palette-icon"
+          <ColorPaletteButton
             label="Change Note Color"
             handleClick={() => showModal({ color: true })}
           />
-          <Button
-            className={`lower-toolbar-button ${
-              noteData.isArchived ? 'unarchive-icon' : 'archive-icon'
-            }`}
+          <ArchiveButton
+            isArchived={noteData.isArchived}
             label={`${noteData.isArchived ? 'Unarchive note' : 'Archive note'}`}
             handleClick={() => handleDataChange({ isArchived: !noteData.isArchived })}
           />
-          <Button
-            className="lower-toolbar-button menu-icon"
+          <MenuButton
             label="Open Menu"
             handleClick={() => showModal({ menu: true })}
           />
         </>
       )}
-    </div>
+    </NoteLowerToolbar>
   );
 }
 
