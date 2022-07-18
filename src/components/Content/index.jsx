836291f527ecff088047ed3faefb8ba4bddcdc43
Renan Torres <rtorres@daitan.com>
2022-07-18 13:13:43 -0300

    [GKR]: Styled Content, Header and Color components

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,17 +1,23 @@
 import React, { useContext } from 'react';
+import styled from 'styled-components';
 import NoteItemsList from '../NoteItemsList';
 import TakeNewNotesHeader from '../TakeNewNotesHeader';
-import './styles.css';
 import NotesAreaHeader from '../NotesAreaHeader';
 import { NotesContext } from '../contexts/NotesProvider';
+
+const StyledContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+`;
 
 export default function Content() {
   const { sidebarSelected } = useContext(NotesContext);
   return (
-    <div className="content">
+    <StyledContent>
       {sidebarSelected !== 'TRASH' && <TakeNewNotesHeader />}
       <NotesAreaHeader />
       <NoteItemsList />
-    </div>
+    </StyledContent>
   );
 }
---
+++ src/components/Header/styles.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import Button from '../Button';
+import searchIcon from '../../assets/svg/search-icon.svg';
+import openNewTabIcon from '../../assets/svg/open-new-tab-icon.svg';
+import closeIcon from '../../assets/svg/close-icon.svg';
+
+export const AppHeader = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid var(--c-light-gray);
+  background-color: var(--c-white);
+  position: fixed;
+  width: 100%;
+  padding: 0.5rem;
+  z-index: 500;
+`;
+
+export const HeaderIcons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const StyledButton = styled(Button)`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1.25rem;
+  width: 1.25rem;
+  margin: 3px;
+  padding: 1.25rem;
+  border-radius: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  &:hover {
+    background-color: var(--c-light-gray);
+  }
+`;
+
+export const SearchButton = styled(StyledButton)`
+  background-image: url(${searchIcon});
+`;
+
+export const OpenNewTabButton = styled(StyledButton)`
+  background-image: url(${openNewTabIcon});
+`;
+
+export const CloseButton = styled(StyledButton)`
+  background-image: url(${closeIcon});
+`;
